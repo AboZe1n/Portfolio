29d5397ad98027c547a92c242e1c1519a643ff99
aboze1n <midozean123@gmail.com>
2025-09-30 22:54:13 +0300

    added "build": "ng build --configuration production"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Portfolio } from './components/portfolio/portfolio';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    Header,
+    Hero,
+    About,
+    Skills,
+    Portfolio,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Portfolio');
+  title = 'Portfolio'; 
 }
+
